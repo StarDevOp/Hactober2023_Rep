076552bdf2bae9c019c1eaafeff6611d0f33328e
Amit Tripathi <Amittripathi2233@gmail.com>
2022-10-19 00:31:06 +0530

    Update Sidebar.jsx

--- lyriks/src/components/Sidebar.jsx
+++ lyriks/src/components/Sidebar.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
 const Sidebar = () => (
-  <div>Sidebar</div>
+   const [mobileMenuOpen, setMobileMenuOpen]=useState(false);
+   return (<>
+    <div></div>
+   </>)
 );
 
 export default Sidebar;
