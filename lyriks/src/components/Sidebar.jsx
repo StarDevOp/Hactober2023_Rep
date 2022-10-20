f1ad5ad81148a34e6af7727dd8e4feaeed9bc63e
chandanDevKiet <116246595+chandanDevKiet@users.noreply.github.com>
2022-10-20 10:07:32 +0530

    Update Sidebar.jsx

--- lyriks/src/components/Sidebar.jsx
+++ lyriks/src/components/Sidebar.jsx
@@ -24,9 +24,11 @@ const Sidebar = () => {
     </div>
 
     <div className="absolute md:hidden block top-6 right-3">
-       
+        {mobileMenuOpen?(<RiCloseLine className="w-6 h-6 text-white mr-2" onClick={()=>setMobileMenuOpen(false)}/>): <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={()=>setMobileMenuOpen(true)} />}
     </div>
 
+   
+    
     
 
    </>
