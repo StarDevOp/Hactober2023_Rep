125fea787c82496233d636dcf325090606890a2a
Gunjan27kardam <108973611+Gunjan27kardam@users.noreply.github.com>
2022-10-30 11:57:06 +0530

    Update TopPlay.jsx

--- lyriks/src/components/TopPlay.jsx
+++ lyriks/src/components/TopPlay.jsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { FreeMode } from "swiper";
 const TopPlay = () => (
   <div>TopPlay</div>
 );
